3cbe9a71b1ad450791a15e3c7ff3588209838636
ashleyfuller <atkfuller@gmail.com>
2025-05-08 13:42:31 -0600

    knight tests passed same code as Bishop but directions changed time for testing

---
+++ shared/src/main/java/chess/RookMove.java
@@ -0,0 +1,40 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class RookMove implements MoveCalc{
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves= new ArrayList<>();
+        int[][] directions={
+                {1,0}, {-1,0}, {0,-1}, {0,1}
+        };
+        int row=myPosition.getRow();
+        int col=myPosition.getColumn();
+        ChessPiece myPiece=board.getPiece(myPosition);
+        for(int dir[]: directions) {
+            int r = row + dir[0];
+            int c = col + dir[1];
+            ChessPosition newPosition = new ChessPosition(r, c);
+            while(board.inBound(newPosition)){
+                if(board.emptyPosition(newPosition)) {
+                    ChessMove move = new ChessMove(myPosition, newPosition, null);
+                    moves.add(move);
+                }
+                else if(board.enemyPiece(newPosition, myPiece)){
+                    ChessMove move = new ChessMove(myPosition, newPosition, null);
+                    moves.add(move);
+                    break;
+                }else{
+                    break;
+                }
+                r = r + dir[0];
+                c = c + dir[1];
+                newPosition= new ChessPosition(r,c);
+            }
+        }
+        return moves;
+    }
+}
